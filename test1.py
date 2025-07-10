6380898b9e355a077fd34e5827bf4029ccb30659
Michael Kadzioch <michael@kadzioch.net>
2025-07-10 12:27:25 +0200

    first exsample

--- test1.py
+++ test1.py
@@ -1,1 +1,6 @@
-print('Hallo Welt')
+import test2 as t2
+
+
+if __name__ == "__main__":
+    print('Hallo Welt')
+    print(__name__)
---
+++ test2.py
@@ -0,0 +1,7 @@
+def irgendwas_passiert():
+    print('Ausgabe aus Funktion')
+
+
+if __name__ == "__main__":
+  print(__name__)
+  irgendwas_passiert()
